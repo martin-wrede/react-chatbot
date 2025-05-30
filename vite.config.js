e214f65bf2aed66ee8df7bed0030b4136f5fd229
martin-wrede <martin.wrede@targetx.de>
2025-05-30 12:07:29 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/targetx-chatbot/", // Ensure this matches your repo name
+  base: "/", // Ensure this matches your repo name
   plugins: [react()],
 });
